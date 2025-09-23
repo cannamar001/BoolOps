024868328bd112f40cebe8bcfb49c480da3fae45
Marcos Cannady <cannamar001@warren.k12.in.us>
2025-09-23 12:19:48 -0400

    Initial commit

---
+++ script.py
@@ -0,0 +1,101 @@
+# age = int(input("Enter your age: "))
+#
+# if age == 16:
+#     print("You are exactly 16 years old.")
+# else:
+#     print("You are not 16 years old.")
+#
+# favorite_color = input("Enter favorite color: ")
+# if favorite_color == "red":
+#     print("You favorite color is red.")
+# else:
+#     print("You favorite color is not red.")
+#
+# grade = int(input("Enter your grade: "))
+#
+# if grade == 90:
+#     print("You don't have a 90.")
+# else:
+#     print("You have a 90.")
+#
+# temperature = int(input("Enter your temperature: "))
+# if temperature > 90:
+#     print("Its warm outside!")
+# else:
+#     print("Its cool outside!")
+#
+# age = int(input("Enter your age: "))
+# if age < 18:
+#     print("You cannot vote.")
+# else:
+#     print("You can vote.")
+#
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You can vote.")
+# else:
+#     print("You cannot vote.")
+#
+# is_sunny = True
+# if not is_sunny:
+#     print("It is not sunny.")
+# else:
+#     print("It is sunny.")
+#
+# is_sunny = True
+# is_rainy = False
+# if is_sunny and is_rainy:
+#     print("There is a rainbow.")
+# else:
+#     print("There no rainbow.")
+#
+# is_holiday = True
+# is_weekend = False:
+# if is_holiday or is_weekend:
+#     print("We don't have school.")
+# else:
+#     print("We have school.")
+
+
+
+
+grade = 85
+if grade >= 90:
+    print("You got a A")
+
+if grade >= 80:
+        print("You got a B")
+
+if grade >= 70:
+    print("You got a C")
+if grade < 70:
+
+    print("You got a F")
+
+
+print()
+print("CHALLENGE 2: WEATHER ADVISOR")
+print("-" * 40)
+print("Create a program that gives clothing advice based on weather conditions.")
+print("Rules:")
+print("  If temperature > 75 AND not raining: 'Wear shorts and a t-shirt!'")
+print("  If temperature <= 32 OR snowing: 'Wear a heavy coat and boots!'")
+print("  Otherwise: 'Dress comfortably!'")
+print()
+
+is_raining = False
+is_snowing = True
+temperature = int(input("Enter your temperature: "))
+if temperature > 75 and raining:
+    print("Wear shorts and a t-shirt")
+else:
+    print("Wear a jacket and boots")
+
+ if temperature <= 32 or is_snowing:
+    print("Wear a heavy coat and boots")
+else:
+    print("Dress comfortably!")
+
+
+
+
